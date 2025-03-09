e9c091c995e670b152c9f7b2723ef6f7ee4473a2
SzymonHarajda <szymonharajda@gmail.com>
2025-03-09 20:17:06 +0100

    about page update

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -19,37 +19,33 @@ const About = () => {
       <div ref={container} className={styles.aboutPage}>
         <section className={styles.about}>
           <div className={styles.aboutContainer}>
+            </div>
+            <div className={styles.aboutText}>
+              <h2>
+                <strong>tmvisual </strong>is the way to make your project unique.
+              </h2>
+              <p>
+                We are a small studio based in Gdańsk, Poland. Founded by a
+                3D artist - Tomasz Michałek
+              </p>
+              <p>
+                With background in architecture we specialize in crafting photorealistic visuals that bring spaces,
+                interiors, and products to life.
+                Whether your dream idea should be projected as an image or animation – we are ready to make it real.
+              </p>
+              <p>
+                For inquiries, collaborations, or to explore latest work, feel free to
+                get in touch.
+              </p>
+            </div>
             <div className={styles.imageContainer}>
               <Image
-                width={400}
-                height={400}
-                src="/hero.png"
+                width={650}
+                height={350}
+                src="/12th_floor_roofbar.jpg"
                 alt="Tomasz Michałek"
                 className={styles.image}
               />
-            </div>
-            <div className={styles.aboutText}>
-              <p>
-                Tomasz Michałek is a 3D Artist based in Gdańsk, Poland. With a
-                background in architecture, he specializes in crafting
-                photorealistic visualizations that bring spaces, interiors, and
-                products to life. His work seamlessly blends technical precision
-                with artistic vision, helping brands and designers transform
-                ideas into immersive visual experiences.
-              </p>
-              <p>
-                Driven by a passion for detail and realism, Tomasz collaborates
-                with architects, interior designers, and product developers to
-                create compelling CGI renderings that tell a story. His
-                expertise extends to lighting, textures, and composition,
-                ensuring each project captures the essence of its subject with
-                clarity and depth.
-              </p>
-              <p>
-                For inquiries, collaborations, or to explore his latest work,
-                feel free to get in touch.
-              </p>
-            </div>
           </div>
         </section>
         <motion.div style={{ height }} className={styles.circleContainer}>
