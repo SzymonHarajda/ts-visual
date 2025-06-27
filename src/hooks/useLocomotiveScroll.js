ec96280ff5dd11a5634a602bb6e0da492127ebb0
Familia <familia@MacBook-Pro-Familia.local>
2025-06-27 13:07:21 +0200

    test change for telefon

--- src/hooks/useLocomotiveScroll.js
+++ src/hooks/useLocomotiveScroll.js
@@ -1,24 +1,27 @@
 "use client";
-import { useEffect } from 'react';
-import LocomotiveScroll from 'locomotive-scroll';
+import { useEffect } from "react";
+import LocomotiveScroll from "locomotive-scroll";
 
 const useLocomotiveScroll = () => {
-    useEffect(() => {
-        const scroll = new LocomotiveScroll({
-            el: document.querySelector('[data-scroll-container]'),
-            smooth: true,
-            smartphone: {
-                smooth: true
-            },
-            tablet: {
-                smooth: true
-            }
-        });
-        document.body.style.cursor = "default";
-        return () => {
-            if (scroll) scroll.destroy();
-        };
-    }, []);
+  useEffect(() => {
+    const scroll = new LocomotiveScroll({
+      el: document.querySelector("[data-scroll-container]"),
+      smooth: true,
+      smartphone: {
+        smooth: true,
+        breakpoint: 767,
+      },
+      tablet: {
+        smooth: false,
+        breakpoint: 1024,
+      },
+      touchMultiplier: 2,
+    });
+    document.body.style.cursor = "default";
+    return () => {
+      if (scroll) scroll.destroy();
+    };
+  }, []);
 };
 
 export default useLocomotiveScroll;
